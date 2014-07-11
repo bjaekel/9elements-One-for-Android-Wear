bb06a996a126f6c1319f3dba5f7201f4e4ba6e5d
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2014-07-11 20:18:30 +0200

    Added apk

--- mobile/src/main/java/com/nineelements/nineelementsonewatch/NineWatchActivity.java
+++ mobile/src/main/java/com/nineelements/nineelementsonewatch/NineWatchActivity.java
@@ -4,12 +4,44 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.Wearable;
+
+
 public class NineWatchActivity extends Activity {
+    private String TAG = "NineWatchActivity";
+
+    private GoogleApiClient googleApiClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.v("9elements One", "onCreate() does nothing since it's a wear app");
 
         setContentView(R.layout.activity_nine_watch);
+
+        //  Needed for communication between watch and device.
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle connectionHint) {
+                        Log.d(TAG, "onConnected: " + connectionHint);
+                    }
+                    @Override
+                    public void onConnectionSuspended(int cause) {
+                        Log.d(TAG, "onConnectionSuspended: " + cause);
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult result) {
+                        Log.d(TAG, "onConnectionFailed: " + result);
+                    }
+                })
+                .addApi(Wearable.API)
+                .build();
+
+        googleApiClient.connect();
     }
 }
